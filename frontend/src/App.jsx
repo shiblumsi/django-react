0840b27027d596b486d4bd904d752d47727dac9e
shiblu <shiblu.msi@gmail.com>
2024-02-12 18:23:40 +0600

    login signup pagecreated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Persons from './pages/Persons'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
 
 return (
     <>
       
-      <Persons />
+      <Signup />
+      <Login />
     </>
   )
 }
