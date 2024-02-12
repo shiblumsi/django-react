60919d46a422335c4b0b26c3eab10a2202e21afc
shiblu <shiblu.msi@gmail.com>
2024-02-12 18:21:54 +0600

    account signup signin api created

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .serializers import UserRegistrationSerializer, ReS
+from django.contrib.auth import authenticate, login, logout
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -7,23 +7,36 @@ from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
 
 
-# Create your views here.
-
-class UserRegistrationView(APIView):
-
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserRegistrationSerializer(users, many=True)
-        return Response({"users": serializer.data})
-
-    def post(self, request):
-        serializer = UserRegistrationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"Success": serializer.data}, status=status.HTTP_201_CREATED)
-        return Response({"Error": serializer.data}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UserRegistrationView2(CreateAPIView):
-    serializer_class = ReS
+class UserRegistrationView(CreateAPIView):
+    serializer_class = UserRegistrationSerializer
     queryset = User.objects.all()
+
+
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        print(username,password,request,request.data)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({"detail": "Login successful."}, status=status.HTTP_200_OK)
+        else:
+            return Response({"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+    # def post(self, request):
+    #     serializer = UserLoginSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         print(serializer.data.get('username'), serializer.data.get('password'))
+    #         user = authenticate(request, username=serializer.data.get('username'), password=serializer.data.get('password'))
+    #         print('true' if user else 'false')
+    #         if user is not None:
+    #             login(request, user)
+    #             return Response({"status": "Login Success"}, status=status.HTTP_200_OK)
+    #     return Response({"status": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"detail": "Logged out successfully."}, status=status.HTTP_200_OK)
